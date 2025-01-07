809526f8508e018667a94b49174b4ab547793baf
Omar Mohumed <wadaniinarais1@gmail.com>
2025-01-07 12:18:51 -0800

    task done

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it does not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the hbnb_dev_db database to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
